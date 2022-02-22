a2a6523d8f1452b5ac66cedfd7fa4b00f1298e58
Leonardo Segars <lsegars@gmail.com>
2022-02-22 07:33:14 -0300

    Add new feature dotenv to set port configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import app from './app';
+import dotenv from 'dotenv';
+
+
+//if (process.env.NODE_ENV === 'development') {
+	
+dotenv.config();
 
 //servicio de la nube process.env.port sino el por defecto el 4800
-var port = process.env.DEV_PORT  || 4000; 
+var port = process.env.PORT  || 4000; 
 
 async function  main () {
     await app.listen(port);
